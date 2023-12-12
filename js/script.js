0f1643e992b3bdae25bb8cc9dd3e40685db46b39
amanbhagat41 <34489796+amanbhagat41@users.noreply.github.com>
2023-12-12 12:49:41 -0500

    bonus squares

--- js/script.js
+++ js/script.js
@@ -39,10 +39,6 @@ var scrabbleTiles = {
   creator: "Ramon Meza",
 };
 let handSize = 7;
-// var scrabbleImg = ["Scrabble_Tile_A.jpg","Scrabble_Tile_B.jpg" ,"Scrabble_Tile_C.jpg", "Scrabble_Tile_D.jpg","Scrabble_Tile_E.jpg","Scrabble_Tile_F.jpg",
-// "Scrabble_Tile_G.jpg","Scrabble_Tile_H.jpg","Scrabble_Tile_I.jpg", "Scrabble_Tile_J.jpg","Scrabble_Tile_K.jpg","Scrabble_Tile_L.jpg", "Scrabble_Tile_M.jpg",
-// "Scrabble_Tile_N.jpg", "Scrabble_Tile_O.jpg","Scrabble_Tile_P.jpg","Scrabble_Tile_Q.jpg","Scrabble_Tile_R.jpg","Scrabble_Tile_S.jpg","Scrabble_Tile_T.jpg",
-// "Scrabble_Tile_U.jpg","Scrabble_Tile_V.jpg","Scrabble_Tile_W.jpg","Scrabble_Tile_X.jpg","Scrabble_Tile_Y.jpg","Scrabble_Tile_Z.jpg","Scrabble_Tile_Blank.jpg"]
 let totalScore = 0;
 $(document).ready(function () {
   
@@ -70,15 +66,19 @@ function getHand() {
 
 $("#submitWord").on("click", function () {
   let wordScore = 0;
-  let doubleLetter = false
-  let doubleWord =false
+  
   $(".onTheboard img").each(function () {
     const tileLetter = $(this).attr('class'); // Get the letter of the current tile
     const tile = scrabbleTiles.pieces.find(tile => tile.letter === tileLetter);
 
     let tileValue = tile.value;
+    if(doubleLetter){
+      
+    }
     wordScore += tileValue;
-    
+    if(doubleWord){
+      wordScore*=2;
+    }
   });
 
   totalScore += wordScore; // Update the total score
@@ -117,11 +117,7 @@ $("#fillHand").on("click", function () {
 $("#restartGame").on("click", function () {
   location.reload();
 });
-// $(function() {
-//   jQuery("#draggable").draggable({
 
-// });
-// });
 $(".tile").draggable({
   scope: "demoBox",
   revertDuration: 100,
@@ -136,7 +132,8 @@ $(".tile").draggable({
   },
 });
 
-
+let doubleLetter = false
+let doubleWord =false
 tilesPlaces = []
 let currentIndex = 0;
 $( "#board1, #board2, #board3, #board4, #board5, #board6, #board7, .tiles").droppable({
@@ -150,6 +147,13 @@ $( "#board1, #board2, #board3, #board4, #board5, #board6, #board7, .tiles").drop
   drop: function (event, ui) {
     $(this).addClass('onTheboard');
     currentIndex = parseInt(this.id.slice(5), 10);
+    if(currentIndex === 6){
+      doubleLetter =true;
+    }
+    if(currentIndex === 2){
+      doubleWord =true;
+    }
+    console.log(doubleWord)
     // console.log(currentIndex)
     if (tilesPlaces.length === 0) {
       $(".tile").draggable("option", "revert", false);
